af5d5ee7cc97f35b6cf9724f6440cb6498166651
Allison Tzadok <allisontzadok@gmail.com>
2017-12-05 05:17:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
-  var array = [];{
-    for (var i = 0; i < musicians.length; i++)
-    return array.push(`${musicians[i]} plays ${instruments[i]}`)}
+  var array = [];
+    for (var i = 0; i < musicians.length; i++){
+    return array.push(`${musicians} plays ${instruments}`);}
 array;
 }
